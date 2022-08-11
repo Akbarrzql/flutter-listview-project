36d7b6eadefa263679c1dec21593e4a88080f3b5
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2022-08-11 08:29:04 +0700

    style: [AR] add feture tabbar in navbar and delete list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listview_project/Tabs.dart';
 import 'package:listview_project/listview.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const listview(),
+      home: const Tabs(),
     );
   }
 }
